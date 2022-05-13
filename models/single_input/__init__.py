7291173e38111a1a4e23b239c83a63f98b8fd2d5
Xinyuan.song <songxinyuan@pku.edu.cn>
2022-05-13 10:45:53 +0800

    first commit

---
+++ models/single_input/__init__.py
@@ -0,0 +1,54 @@
+
+from .cnn_test import cnn_test
+from .cnn_test2 import cnn_test2
+
+from .cnn_test import  cnn_ordinary_1, cnn_ordinary_pointwise_1,cnn_pointwise_1,cnn_ordinary_max_1,cnn_ordinary_max_pointwise_1
+from .rnn_test import rnn_4layer, rnn_3layer, rnn_2layer_fc,rnn_4layer_fc,rnn_3layerGRU_fc
+from .cnn_test import cnn_ordinary_2_pointwise_1,cnn_ordinary_2,cnn_ordinary_ordinary_1,cnn_ordinary_ordinary_pointwise_1
+from .cnn_test import cnn_ordinary_ordinary_fc_1,cnn_ordinary_ordinary_fc_pointwise_1
+from .rnn_sxy import rnn_sxy_1, rnn_sxy_1_false, rnn_sxy_2, rnn_sxy_2_false
+
+from .rnn_cell_sxy.LSTMcell_sxy import LSTMcell
+from .rnn_cell_sxy.GRUcell_sxy import GRUcell
+from .rnn_cell_sxy.RNNcell_sxy import RNNcell
+from .rnn_cell_sxy.LSTMCcell_sxy import LSTMCcell
+
+from .rnn_sxy import rnn_sxy_4, rnn_sxy_4_false,rnn_sxy_5, rnn_sxy_6,rnn_sxy_7,rnn_sxy_8,rnn_sxy_9,rnn_sxy_10,rnn_sxy_11,rnn_sxy_12,\
+                        rnn_sxy_13,rnn_sxy_14,rnn_sxy_15,rnn_sxy_16,rnn_sxy_17,rnn_sxy_18,rnn_sxy_19,rnn_sxy_20,rnn_sxy_21,rnn_sxy_22,\
+                        rnn_sxy_23,rnn_sxy_24,rnn_sxy_25,rnn_sxy_26,rnn_sxy_27,rnn_sxy_28,rnn_sxy_29,rnn_sxy_30,rnn_sxy_31,rnn_sxy_32,\
+                        rnn_sxy_33,rnn_sxy_34,rnn_sxy_35,rnn_sxy_36,rnn_sxy_37,rnn_sxy_38,rnn_sxy_39,rnn_sxy_40,rnn_sxy_41,rnn_sxy_42,\
+                        rnn_sxy_43,rnn_sxy_44,rnn_sxy_45,rnn_sxy_46,rnn_sxy_47,rnn_sxy_48,rnn_sxy_49,rnn_sxy_50
+
+
+
+
+
+from .cnn2d_sxy import cnn2d_sxy_1,cnn2d_sxy_2,cnn2d_sxy_3,cnn2d_sxy_4,cnn2d_sxy_5,cnn2d_sxy_6,cnn2d_sxy_7,cnn2d_sxy_8
+
+from .cnn1d_sxy import cnn1d_sxy_1,cnn1d_sxy_2,cnn1d_sxy_3,cnn1d_sxy_4,cnn1d_sxy_5,\
+                       cnn1d_sxy_6,cnn1d_sxy_7,cnn1d_sxy_8,cnn1d_sxy_9,cnn1d_sxy_10,\
+                       cnn1d_sxy_11,cnn1d_sxy_12,cnn1d_sxy_13,cnn1d_sxy_14,cnn1d_sxy_15,\
+                       cnn1d_sxy_16,cnn1d_sxy_17,cnn1d_sxy_18,cnn1d_sxy_19,cnn1d_sxy_20,\
+                       cnn1d_sxy_21,cnn1d_sxy_22,cnn1d_sxy_23,cnn1d_sxy_9_false,cnn1d_sxy_24,\
+                       cnn1d_sxy_14_2,cnn1d_sxy_14_cat,cnn1d_sxy_25
+
+from .cnn1d_sxy import cnn1d_sxy_1_false,cnn1d_sxy_1_false12,cnn1d_sxy_2_false,cnn1d_sxy_2_false490
+
+
+
+from .cnn1d_sxy import cnn1d_sxy_1,cnn1d_sxy_2,cnn1d_sxy_3,cnn1d_sxy_4
+
+from .rnn_sxy_2 import rnn_sxy_2_1,rnn_sxy_2_2,rnn_sxy_2_3,rnn_sxy_2_4,rnn_sxy_2_5,rnn_sxy_2_6,rnn_sxy_2_7,rnn_sxy_2_8, rnn_sxy_2_9,\
+                       rnn_sxy_2_10,rnn_sxy_2_11,rnn_sxy_2_12,rnn_sxy_2_13,rnn_sxy_2_14,rnn_sxy_2_15,\
+                       rnn_sxy_2_16,rnn_sxy_2_17,rnn_sxy_2_18,rnn_sxy_2_19,rnn_sxy_2_20,rnn_sxy_2_21,rnn_sxy_2_22,rnn_sxy_2_23,\
+                       rnn_sxy_2_24,rnn_sxy_2_25,rnn_sxy_2_26,rnn_sxy_2_27,rnn_sxy_2_28,rnn_sxy_2_29,rnn_sxy_2_30,rnn_sxy_2_31,\
+                       rnn_sxy_2_32,rnn_sxy_2_33,rnn_sxy_2_34,rnn_sxy_2_35,rnn_sxy_2_36,rnn_sxy_2_37,rnn_sxy_2_38,rnn_sxy_2_39,\
+                       rnn_sxy_2_40,rnn_sxy_2_41,rnn_sxy_2_42,rnn_sxy_2_43,rnn_sxy_2_44,rnn_sxy_2_45,rnn_sxy_2_46,rnn_sxy_2_47,\
+                       rnn_sxy_2_48,rnn_sxy_2_49,rnn_sxy_2_50,rnn_sxy_2_51,rnn_sxy_2_52,rnn_sxy_2_53,rnn_sxy_2_54,rnn_sxy_2_55,rnn_sxy_2_56,\
+                       rnn_sxy_2_57,rnn_sxy_2_58,rnn_sxy_2_59,rnn_sxy_2_60,rnn_sxy_2_61,rnn_sxy_2_62,rnn_sxy_2_63,rnn_sxy_2_64,rnn_sxy_2_65,\
+                       rnn_sxy_2_66,rnn_sxy_2_67,rnn_sxy_2_68,rnn_sxy_2_69,rnn_sxy_2_70,rnn_sxy_2_71,rnn_sxy_2_72,rnn_sxy_2_73,\
+                       rnn_sxy_2_74,rnn_sxy_2_75,rnn_sxy_2_76
+
+from .rnn_sxy_2 import rnnmultilayers
+
+from .transformers import transformer_sxy_1
